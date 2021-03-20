e77a3da79e0d6aeb105c9dd465c358969b052600
Paweł Sapek <psapek@infotower.pl>
2021-03-20 18:48:50 +0100

    made actors page

--- app/src/main/java/com/example/movieappcompose/screens/actors/ActorsPage.kt
+++ app/src/main/java/com/example/movieappcompose/screens/actors/ActorsPage.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -14,10 +15,13 @@ import androidx.compose.material.icons.sharp.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.movieappcompose.R
 import com.example.movieappcompose.data.models.Movie
 import com.example.movieappcompose.screens.mainActivity.MainActivityViewModel
 import com.example.movieappcompose.ui.Dimen
+import com.example.movieappcompose.utlis.LocalActions
 import com.example.movieappcompose.widgets.MovieTabRow
 import com.example.movieappcompose.widgets.Page
 import com.example.movieappcompose.widgets.pager_temp.ExperimentalPagerApi
@@ -31,15 +35,18 @@ fun ActorsPage(
     movie: Movie,
     mainActivityViewModel: MainActivityViewModel = viewModel(),
 ) {
-    val tabs = listOf("Cast", "Crew")
+    val tabs = listOf(stringResource(R.string.cast), stringResource(R.string.crew))
     val pagerState = rememberPagerState(pageCount = tabs.size)
     val coroutineScope = rememberCoroutineScope()
+    val upPress = LocalActions.current.upPress
 
     Page(showBottomBar = mainActivityViewModel.showBottomNavigationBar) {
         Column {
             Row(Modifier.padding(horizontal = Dimen.padding.medium, vertical = Dimen.padding.big),
                 verticalAlignment = Alignment.CenterVertically) {
-                Icon(Icons.Sharp.ArrowBack, "Go back")
+                IconButton(onClick = { upPress() }) {
+                    Icon(Icons.Sharp.ArrowBack, stringResource(R.string.go_back))
+                }
                 Spacer(modifier = Modifier.padding(start = Dimen.margin.big))
                 Text(text = movie.title, style = MaterialTheme.typography.h2)
             }
--- app/src/main/java/com/example/movieappcompose/screens/actors/PersonItem.kt
+++ app/src/main/java/com/example/movieappcompose/screens/actors/PersonItem.kt
@@ -3,6 +3,7 @@ package com.example.movieappcompose.screens.actors
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -10,69 +11,113 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.example.movieappcompose.R
 import com.example.movieappcompose.ui.Dimen
 import com.example.movieappcompose.ui.MovieColors
 import com.example.movieappcompose.utlis.imageWidth500Url
-import com.example.movieappcompose.widgets.Center
 import dev.chrisbanes.accompanist.glide.GlideImage
+
 
 @Composable
 fun PersonItem(person: Person) {
-    Row(
-        Modifier, verticalAlignment = Alignment.CenterVertically
-    ) {
-        GlideImage(
-            data = imageWidth500Url(person.profilePicture),
-            contentDescription = stringResource(id = R.string.actor_photo_description, person.name),
-            modifier = Modifier
-                    .weight(1.5f)
-                    .padding(Dimen.padding.big)
-                    .aspectRatio(Dimen.squareRatio)
-                    .clip(
-                        CircleShape
-                    ),
-            error = {
-                Center {
-                    Box(
-                        modifier = Modifier
-                                .fillMaxSize()
-                                .background(MovieColors.backgroundEnd),
-                    )
-                }
-            },
-            loading = {
-                Box(
-                    modifier = Modifier
-                            .fillMaxSize()
-                            .background(MovieColors.backgroundEnd),
-                ) {
-                    CircularProgressIndicator(Modifier.align(Alignment.Center))
-                }
-            },
-            contentScale = ContentScale.Crop
-        )
-        Column(
-            modifier = Modifier
-                    .weight(5f)
-                    .fillMaxHeight()
-                    .padding(Dimen.padding.medium),
-            verticalArrangement = Arrangement.SpaceEvenly,
+    BoxWithConstraints {
+        ConstraintLayout(
+            Modifier
+                    .padding(maxWidth.times(0.03f))
+                    .clip(RoundedCornerShape(10.dp))
+                    .padding(maxWidth.times(0.03f))
         ) {
-            Text(
-                text = person.name,
-                style = MaterialTheme.typography.h4.copy(fontSize = 18.sp),
-                textAlign = TextAlign.Center,
+            val (poster, card, info) = createRefs()
+
+            Box(modifier = Modifier
+                    .requiredWidth(this@BoxWithConstraints.maxWidth.times(0.2f))
+                    .zIndex(5.dp.value)
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+                    .shadow(elevation = 10.dp, shape = CircleShape)
+                    .constrainAs(poster) {
+                        top.linkTo(parent.top)
+                        start.linkTo(card.start,
+                            margin = this@BoxWithConstraints.maxWidth.times(0.045f))
+                        bottom.linkTo(info.bottom, margin = Dimen.margin.big)
+                    }) {
+                // TODO: 05/11/2020 add placeholder and loading image
+                GlideImage(
+                    data = imageWidth500Url(person.profilePicture),
+                    contentDescription = stringResource(id = R.string.poster_description,
+                        person.name),
+                    contentScale = ContentScale.Crop,
+                    error = {
+                        Box(
+                            modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(MovieColors.backgroundEnd),
+                        )
+                    },
+                    loading = {
+                        Box(
+                            modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(MovieColors.backgroundEnd),
+                        ) {
+                            CircularProgressIndicator(Modifier.align(Alignment.Center))
+                        }
+                    },
+                )
+
+            }
+
+            Box(
+                Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp))
+                        .zIndex(4.dp.value)
+                        .background(Color.White)
+                        .constrainAs(card) {
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            top.linkTo(info.top)
+                            bottom.linkTo(info.bottom)
+                            height = Dimension.fillToConstraints
+                        }
             )
-            Text(
-                text = person.position,
-                style = MaterialTheme.typography.h5.copy(fontSize = 15.sp),
-                textAlign = TextAlign.Center,
-            )
+
+            Column(
+                Modifier
+                        .padding(vertical = 15.dp)
+                        .zIndex(5.dp.value)
+                        .constrainAs(info) {
+                            start.linkTo(poster.end,
+                                margin = this@BoxWithConstraints.maxWidth.times(0.045f))
+                            end.linkTo(card.end, margin = Dimen.margin.medium)
+                            bottom.linkTo(poster.bottom)
+                            width = Dimension.fillToConstraints
+                        },
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    textAlign = TextAlign.Start,
+                    text = person.name,
+                    style = MaterialTheme.typography.h2,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Text(
+                    text = person.position,
+                    style = MaterialTheme.typography.subtitle1
+                )
+            }
         }
     }
 }
